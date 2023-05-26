4350db1d0f74889119936e515847a9bb69a038cd
wngkyle <99611120+wngkyle@users.noreply.github.com>
2023-05-25 21:59:49 -0700

    array

--- topics/array.js
+++ topics/array.js
@@ -5,5 +5,89 @@
 // You can store different types of object in javascript array
 let selectedColors = ['red', 'blue'];
 selectedColors[2] = 'green';
-selectedColors[3] = 8;
+selectedColors[5] = 8;
 console.log(selectedColors.length);
+console.log(selectedColors)
+
+// Common practice in javascript to declare arrays with const
+const cars = [];
+cars[0] = "BMW";
+cars[1] = "HONDA";
+
+// You can also use keyword 'new' to declare array but it's not necessary 
+// It does the same thing
+const cars2 = new Array("BMW", "Volvo");
+
+// toString() converts an array into string
+console.log(cars2.toString());
+
+// Arrays are objects
+console.log(typeof(cars2));
+
+// The elements in an javascript array can be other objects
+
+// Array methods
+// .length and .sort()
+// Since length is a property of array, we don't need ()
+console.log(cars.length);
+console.log(cars.sort());
+
+// Adding elements to an array
+// .push()
+// NOTE: adding high indexes can create undefined holes in array
+console.log(cars);
+cars.push("Mercedes");
+// cars[cars.length] = "Mercedes"; // does the same thing
+console.log(cars);
+
+// Difference between arrays and objects:
+// Arrays use numbered indexes
+// Objects use named indexes
+// Use arrays when you want the element names to be number, use objects
+// when you want the element names to be string
+
+// Distinguishing arrays and objects
+console.log(Array.isArray(cars));
+console.log(cars instanceof Array);
+
+// .join() behaves like .toString(), difference is you can specify the seperator
+console.log(cars.join('-'));
+
+// .pop() removes and return the last element in the array
+let lastCar = cars.pop();
+
+cars.push("Aston Martin");
+cars.push("McLaren");
+cars.push("Alfa Romeo");
+cars.push("Porsche");
+cars.push("Renault");
+cars.push("Ford");
+// .shift() removes and return the first element and shift all other elements to a lower index
+let firstCar = cars.shift();
+console.log(cars);
+
+// .unshift() adds a new element to an array (at the beginning), and "unshifts" older elements
+cars.unshift(firstCar);
+console.log(cars);
+
+// keyword 'delete' 
+// NOTE: using 'delete' leaves undefined holes in the array
+delete cars[6];
+console.log(cars);
+
+// .concat() this creates a new array by merging existing arrays
+// NOTE: this array does not modify existing array, it returns a new array.
+// It can also take strings as arguments, and can also take multiple arrays 
+// as arguments
+let cars3 = cars.concat(cars2);
+console.log(cars3);
+
+// .flat() flattens an array, reduces dimensions by creating a new array
+let example = [[1,2],[3,4],[4,5]];
+let newExample = example.flat();
+console.log(example);
+console.log(newExample);
+
+// .splice() used to modify the contents of an array by removing the existing elements and/or by adding new elements
+// ,splice(index, remove_count)
+
--- topics/brief.js
+++ topics/brief.js
@@ -8,9 +8,8 @@ var lname = prompt("Enter your last name: ");
 
 console.log("Your Full Name is", fname, lname);
 
-console.log("hello wold");
 
-// use keyword let for variables
+// Using keyword let for variables
 let firstWord = "kkkk";
 console.log(firstWord);
 
@@ -19,11 +18,15 @@ const annualRate = 0.3; // cannot modify the value later on
 let interestRate = 0.4;
 interestRate = 1;
 console.log(interestRate);
+// annualRate = 0.4; // this line will result in error since annual rate is declared as constant
 
 // undefined variables
 let firstName; // undefined
 let lastName = undefined; // undefined
 let middleName = null; // we use null in situations where we want to explicitly clear the value of the variable
+// Null means empty in javascript, it is a primitive type
+// Null is an object
+// Undefined means has been declared, but has not been assigned a value yet
 
 // Dynamic typing in javascript
 let num1 = 5;
@@ -48,16 +51,18 @@ console.log(typeof(middleName)) // type object
 // In the curly braces, we add key and values pair
 let person = {
     firstName: "Mosh",
-    age: 30
+    age: 0
 }; 
 console.log("person:" + person.age);
 
 // changing the name of the person
 // Dot notation 
 // Bracket notation
-person.name = "John";
+person.name = "John"; // doing this automatically adds a new attribute to the person object
+                      // b/c 'name' is an attribute that has not yet been declared
 console.log(person.name);
 person['age'] = 20;
 console.log(person.age);
+console.log(person);
 
 
